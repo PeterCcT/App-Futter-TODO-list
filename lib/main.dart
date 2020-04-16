214baef3b7d080c4c6f4f164e37db67b6223436c
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-04-16 15:45:12 -0300

    Correção do layout e validação para campo vazio

--- lib/main.dart
+++ lib/main.dart
@@ -36,12 +36,48 @@ class _HomeState extends State<Home> {
   }
 
   void _addTarefa() {
+    if (_tarefaController.text == "") {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text(
+                "Sua tarefa está vazia!",
+              ),
+              content: Text("Coloque alguma coisa nas suas tarefas :)"),
+              actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    })
+              ],
+            );
+          });
+    } else {
+      setState(() {
+        Map<String, dynamic> novaTarefa = Map();
+        novaTarefa["tittle"] = _tarefaController.text;
+        novaTarefa["estado"] = false;
+        _tarefaController.text = "";
+        _todoList.add(novaTarefa);
+        _saveTarefas();
+      });
+    }
+  }
+
+  Future<Null> _recarregar() async {
+    await Future.delayed(Duration(seconds: 1));
     setState(() {
-      Map<String, dynamic> novaTarefa = Map();
-      novaTarefa["tittle"] = _tarefaController.text;
-      novaTarefa["estado"] = false;
-      _tarefaController.text = "";
-      _todoList.add(novaTarefa);
+      _todoList.sort((a, b) {
+        if (a["estado"] && !b["estado"])
+          return 1;
+        else if (!a["estado"] && b["estado"])
+          return -1;
+        else
+          return 0;
+      });
+
       _saveTarefas();
     });
   }
@@ -60,51 +96,51 @@ class _HomeState extends State<Home> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 3, horizontal: 15),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      controller: _tarefaController,
-                      style:
-                          TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
-                      decoration: InputDecoration(
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.indigo,
-                            width: 1.4,
-                          ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 3, horizontal: 15),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _tarefaController,
+                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+                    decoration: InputDecoration(
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.indigo,
+                          width: 1.4,
                         ),
-                        hintText: "Digite sua tarefa",
-                        hintStyle: TextStyle(
-                          fontSize: 15,
-                          fontStyle: FontStyle.italic,
-                        ),
+                      ),
+                      hintText: "Digite sua tarefa",
+                      hintStyle: TextStyle(
+                        fontSize: 15,
+                        fontStyle: FontStyle.italic,
                       ),
                     ),
                   ),
-                  IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: _addTarefa,
-                    iconSize: 40,
-                    splashColor: Colors.lightGreenAccent,
-                  ),
-                ],
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: _addTarefa,
+                  iconSize: 40,
+                  splashColor: Colors.lightGreenAccent,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: RefreshIndicator(
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 20.0),
+                itemCount: _todoList.length,
+                itemBuilder: itemBuilder,
               ),
+              onRefresh: _recarregar,
             ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: ScrollPhysics(),
-              padding: EdgeInsets.only(top: 20.0),
-              itemCount: _todoList.length,
-              itemBuilder: itemBuilder,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -154,6 +190,7 @@ class _HomeState extends State<Home> {
                 }),
             duration: Duration(seconds: 3),
           );
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
